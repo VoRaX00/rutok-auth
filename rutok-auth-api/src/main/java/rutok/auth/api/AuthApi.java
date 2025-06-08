ea69ebe8c51097b34e1fbf34ec36ce6259defb2d
Кержаков Никита <nkerzhakov@atbplugin.tech>
2025-06-08 19:39:33 +1000

    SCRUM-55 Исправление ошибок

--- rutok-auth-api/src/main/java/rutok/auth/api/AuthApi.java
+++ rutok-auth-api/src/main/java/rutok/auth/api/AuthApi.java
@@ -11,18 +11,18 @@ public interface AuthApi {
 
     @PostMapping("/api/login")
     @Operation(summary = "Аутентификация пользователя")
-    ResponseEntity<AuthDto> login(LoginRequest request);
+    ResponseEntity<AuthDto> login(@RequestBody LoginRequest request);
 
     @PostMapping("/api/refresh")
     @Operation(summary = "Обновление пары JWT токенов")
-    ResponseEntity<AuthDto> refresh(TokenRequest request);
+    ResponseEntity<AuthDto> refresh(@RequestBody TokenRequest request);
 
     @PostMapping("/api/logout")
     @Operation(summary = "Удаление активных сессий")
-    void logout(TokenRequest request);
+    void logout(@RequestBody TokenRequest request);
 
     @PostMapping("/api/registration")
     @Operation(summary = "Регистрация пользователя")
-    void registration(RegisterDto registerDto);
+    void registration(@RequestBody RegisterDto registerDto);
 
 }
